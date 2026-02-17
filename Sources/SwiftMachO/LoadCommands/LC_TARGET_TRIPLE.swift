7f8fe470205cc39a99495c7e6e64a166ef59b921
Jon <jonmalm@gmail.com>
2026-02-16 20:17:58 -0500

    Parsing for LC_TARGET_TRIPLE and LC_TWOLEVEL_HINTS

--- Sources/SwiftMachO/LoadCommands/LC_TARGET_TRIPLE.swift
+++ Sources/SwiftMachO/LoadCommands/LC_TARGET_TRIPLE.swift
@@ -8,23 +8,37 @@
 import Foundation
 import BinaryParsing
 
+import Foundation
+import BinaryParsing
+
 public struct LC_TARGET_TRIPLE: LoadCommand {
     public static let expectedID: LoadCommandHeader.ID = .LC_TARGET_TRIPLE
     public let header: LoadCommandHeader
     public let range: Range<Int>
+
+    public let triple: String
 }
 
 extension LC_TARGET_TRIPLE {
     public init(parsing input: inout ParserSpan, endianness: Endianness) throws {
-        self.range = input.parserRange.range
-        
+        let commandStart = input.parserRange.lowerBound
         self.header = try Self.parseAndValidateHeader(from: &input, endianness: endianness)
+
+        self.triple = try String(parsingNulTerminated: &input)
+
+        // Ensure the full cmdsize is consumed
+        let finalPosition = commandStart + Int(self.header.cmdSize)
+        try input.seek(toAbsoluteOffset: finalPosition)
+        self.range = commandStart..<finalPosition
     }
 }
 
 extension LC_TARGET_TRIPLE: Displayable {
-    public var title: String { "\(Self.self) TODO" }
-    public var description: String { "" }
-    public var fields: [DisplayableField] { fieldBuilder().build() }
+    public var description: String { "Target Triple" }
+    public var fields: [DisplayableField] {
+        var b = fieldBuilder()
+        b.add(label: "Triple", stringValue: triple, size: triple.utf8.count + 1)
+        return b.build()
+    }
     public var children: [Displayable]? { nil }
 }
--- Sources/SwiftMachO/LoadCommands/LC_TWOLEVEL_HINTS.swift
+++ Sources/SwiftMachO/LoadCommands/LC_TWOLEVEL_HINTS.swift
@@ -5,26 +5,37 @@
 //  Created by jon on 10/16/25.
 //
 
-import Foundation
 import BinaryParsing
+import Foundation
 
 public struct LC_TWOLEVEL_HINTS: LoadCommand {
     public static let expectedID: LoadCommandHeader.ID = .LC_TWOLEVEL_HINTS
     public let header: LoadCommandHeader
     public let range: Range<Int>
+
+    public let offset: UInt32
+    public let nhints: UInt32
+    // TODO: Need to parse the hints pointed to by offset
 }
 
 extension LC_TWOLEVEL_HINTS {
     public init(parsing input: inout ParserSpan, endianness: Endianness) throws {
         self.range = input.parserRange.range
-        
+
         self.header = try Self.parseAndValidateHeader(from: &input, endianness: endianness)
+
+        self.offset = try UInt32(parsing: &input, endianness: endianness)
+        self.nhints = try UInt32(parsing: &input, endianness: endianness)
     }
 }
 
 extension LC_TWOLEVEL_HINTS: Displayable {
-    public var title: String { "\(Self.self) TODO" }
-    public var description: String { "" }
-    public var fields: [DisplayableField] { fieldBuilder().build() }
+    public var description: String { "Two-level namespace hints" }
+    public var fields: [DisplayableField] {
+        var b = fieldBuilder()
+        b.add(label: "Hints Offset", stringValue: offset.hexDescription, size: 4)
+        b.add(label: "Number of Hints", stringValue: nhints.description, size: 4)
+        return b.build()
+    }
     public var children: [Displayable]? { nil }
 }
